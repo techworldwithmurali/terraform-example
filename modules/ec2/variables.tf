821af6088917bfa2e6f4c6db0e984291699af84e
Moole Muralidhara Reddy <115227391+techworldwithmurali@users.noreply.github.com>
2024-12-09 16:49:05 +0530

    Create variables.tf

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,19 @@
+variable "ami" {
+  description = "Declaring the AMi"
+  type        = string
+}	
+
+variable "instance_type" {
+  description = "Declaring the instance_type"
+  type        = string
+}	
+
+variable "subnet_id" {
+  description = "Declaring the subnet_id"
+  type        = string
+}	
+
+variable "tags" {
+  description = "Declaring the tags"
+  type        = string
+}
