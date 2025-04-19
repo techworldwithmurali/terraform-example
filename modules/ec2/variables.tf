35a7c792e547db4a77fdab10cee5abfd7708f7f0
Tech World with Murali <115227391+techworldwithmurali@users.noreply.github.com>
2025-04-19 11:58:03 +0530

    Update the ec2 variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,11 +1,11 @@
 variable "ami" {
- description = "The AMI ID to use for the EC2 instance"
+ description = "The AMI ID to use for the EC2 instances"
   type        = string
 }
 
 variable "instance_type" {
   description = "The type of the EC2 instance"
-  type        = string
+
 }
 
 variable "subnet_id" {
